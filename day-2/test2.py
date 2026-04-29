b3437befac01c8a4632b86991cc6164c31aab98e
leeseongjin <doyeonlove09@gmail.com>
2026-04-29 16:48:54 +0900

    파이썬 실습

---
+++ day-2/test2.py
@@ -0,0 +1,7 @@
+am_price = 2000
+ca_price = 3000
+capu_price = 3500
+a = int(input("아메리카노 판매 개수: "))*am_price
+b = int(input("카페라테 판매 개수: "))*ca_price
+c = int(input("카푸치노 판매 개수: "))*capu_price
+print("총 매출은",a+b+c,"입니다.")
---
+++ day-2/test3.py
@@ -0,0 +1,11 @@
+money = int(input("투입한 돈: "))
+ob_money = int(input("물건값(100원 단위): "))
+if(money>ob_money):
+    r = money - ob_money
+    d = r//500
+    e = (r%500) // 100 
+    print("거스름돈:",r)
+    print("500원 동전의 개수:",d)
+    print("100원 동전의 개수:",e)
+else:
+    print("금액이 적습니다")    
