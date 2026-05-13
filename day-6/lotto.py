fc3f3ec81afacbb145c19c0351d95d73200c2b6a
leeseongjin <doyeonlove09@gmail.com>
2026-05-13 15:05:44 +0900

    랜덤 함수

---
+++ day-6/lotto.py
@@ -0,0 +1,9 @@
+import random
+def get_lotto():
+    numbers = [] #비어있는 리스트
+    while len(numbers) < 6:
+        n=random.randint(1,45)
+        if n not in numbers: # 중복되면 안됨
+            numbers.append(n) # 뒤에 추가
+    return numbers
+print(get_lotto())
