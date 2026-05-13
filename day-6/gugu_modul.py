1eaa9d15324e858df500886b618e7ba8ff0a8a19
leeseongjin <doyeonlove09@gmail.com>
2026-05-13 16:44:35 +0900

    구구단 모듈 함수 모음

---
+++ day-6/gugu_modul.py
@@ -0,0 +1,11 @@
+def v_gugudan():
+    for i in range(2,10):
+        print(f"({i}단)")
+        for j in range(1,10):
+            print(f"{i} * {j} = {i*j}")
+def h_gugudan():
+    for i in range(2,10):
+        print(f"({i}단)")
+        for j in range(1,10):
+            print(f"{i} * {j} = {i*j}", end=" ")
+    print()
