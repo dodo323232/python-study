7b7c7f823a2d0853e440e802014be525d4678d4f
leeseongjin <doyeonlove09@gmail.com>
2026-05-13 16:46:20 +0900

    EX 함수와 메인

---
+++ day-6/ex01_main.py
@@ -0,0 +1,12 @@
+# import ex01_fun
+# ex01_fun.hello()  #함수호출
+
+# from ex01_fun import * # 상자에서 다 꺼내놨기 때문에 그냥 함수만 써도 됨
+# hello()
+
+# import ex01_fun as f1 # f1 별명
+# f1.hello()
+
+from ex01_fun import hi
+hi()
+# hello() # 오류
